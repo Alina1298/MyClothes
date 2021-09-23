eb7273c02d68b381d56436ffaf197f999749c728
Алина <alina.12.98@mail.ru>
2021-09-23 19:03:34 +0300

    html, css ready

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,16 +1,20 @@
+require('dotenv').config()
 const router = require('express').Router();
-// const axios = require('axios');
+const { Clothes } = require('../models');
+const axios = require('axios');
 
 router.get('/', async (req, res) => {
   try {
-    // const wheather = await axios('https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&lang=ru&appid=105c9fb5895b9a0ce42e7bf8bbf400a0');
+    const wheather = await axios(`https://api.openweathermap.org/data/2.5/weather?q=London&units=metric&lang=ru&appid=${process.env.API_WHEATHER}`);
 
-    // console.log(wheather.data)
-    // const data = wheather.data
-    // const description = data.weather.map(el => el.description).toString();
-    // const icon = data.weather.map(el => el.icon).toString();
-    // res.render('index', { city: data.name, temp: data.main.temp, description: description, icon: icon, min_temp: data.main.temp_min, max_temp: data.main.temp_max, wind_speed: data.wind.speed })
-    res.render('index')
+    console.log(wheather.data)
+    const data = wheather.data
+    const description = data.weather.map(el => el.description).toString();
+    const icon = data.weather.map(el => el.icon).toString();
+    const clothes = await Clothes.findAll();    
+    res.render('index', { city: data.name, temp: data.main.temp, description: description, icon: icon, min_temp: data.main.temp_min, max_temp: data.main.temp_max, wind_speed: data.wind.speed, clothes })
+
+    // res.render('index', { clothes })
   } catch (error) {
     console.log(error)
     res.render('index')
