ad57201c56da5d7f7fb4e86071bee43512d5ffc1
Алина <alina.12.98@mail.ru>
2021-09-24 16:20:27 +0300

    Project is ready

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,37 +4,46 @@ const { Clothes, Category } = require('../models');
 const axios = require('axios');
 const { getCategory, getClothesRandom } = require('../public/js/weatherClothes');
 
-router.get('/', async (req, res) => {
-  try {
-    const wheather = await axios(`https://api.openweathermap.org/data/2.5/weather?q=Istanbul&units=metric&lang=ru&appid=${process.env.API_WHEATHER}`);
-    const data = wheather.data
-    const description = data.weather.map(el => el.description).toString();
-    const icon = data.weather.map(el => el.icon).toString();
-    const date = new Date();
-    const tempFromAPI = data.main.temp;
-    const temp = Number(tempFromAPI.toString().match(/^\d+/gm));
-    let categories = getCategory(temp);
-    let clothesRandom = getClothesRandom(categories)
-    let titleBottom = clothesRandom.bottom;
-    let titleTop = clothesRandom.top;
+router.route('/')
+  .get(async (req, res) => {
+    try {
+      const { city_title } = req.query
+      console.log(city_title)
+      const wheather = await axios(`https://api.openweathermap.org/data/2.5/weather?q=${city_title}&units=metric&lang=ru&appid=${process.env.API_WHEATHER}`);
+      const data = wheather.data
+      const description = data.weather.map(el => el.description).toString();
+      const icon = data.weather.map(el => el.icon).toString();
+      const date = new Date();
+      const tempFromAPI = data.main.temp;
+      const temp = Number(tempFromAPI.toString().match(/^\d+/gm));
+      let categories = getCategory(temp);
+      let clothesRandom = getClothesRandom(categories)
+      let titleBottom = clothesRandom.bottom;
+      let titleTop = clothesRandom.top;
 
-    const categoryTop = await Category.findOne({ where: { title: titleTop } })
-    const categoryBottom = await Category.findOne({ where: { title: titleBottom } })
+      const categoryTop = await Category.findOne({ where: { title: titleTop } })
+      const categoryBottom = await Category.findOne({ where: { title: titleBottom } })
 
-    const clothesTop = await Clothes.findOne({ where: { season: categories.season, categoryId: categoryTop.id } });
-    const clothesBottom = await Clothes.findOne({ where: { season: categories.season, categoryId: categoryBottom.id } });
+      const clothesTop = await Clothes.findOne({ where: { season: categories.season, categoryId: categoryTop.id } });
+      const clothesBottom = await Clothes.findOne({ where: { season: categories.season, categoryId: categoryBottom.id } });
+      
+      const clothes = [clothesTop, clothesBottom]
 
-    const clothes = [clothesTop, clothesBottom]
+      if (clothes.includes(null)) {
+        return res.render('errorClothes', {
+          message: 'Тебе стоит пойти в магазин',
+          error: {}
+        })
+      } else {
+        return res.render('index', { city_title: city_title, temp: data.main.temp, description: description, icon: icon, min_temp: data.main.temp_min, max_temp: data.main.temp_max, wind_speed: data.wind.speed, clothes, date: date })
+      }
+      // const clothes = await Clothes.findAll()
+      // res.render('index', { clothes })
 
-    console.log('clothes', clothes)
-
-    res.render('index', { city: data.name, temp: data.main.temp, description: description, icon: icon, min_temp: data.main.temp_min, max_temp: data.main.temp_max, wind_speed: data.wind.speed, clothes, date: date })
-
-    // res.render('index', { clothes })
-  } catch (error) {
-    console.log(error)
-    res.render('index')
-  }
-})
+    } catch (error) {
+      console.log(error)
+      return res.render('index')
+    }
+  })
 
 module.exports = router;
